d20f1c585193a2db1dbd59a584b43fc445b04c3c
Gungyphant <gungyphant@gmail.com>
2025-12-12 19:35:36 +0000

    Reworked PartTwo

--- Program.cs
+++ Program.cs
@@ -1,20 +1,11 @@
-﻿using System.Xml;
-
-namespace AOC2025Day11
+﻿namespace AOC2025Day11
 {
     public class Program
     {
-        public static string PartOne(string data)
+        public static int NumberOfRoutesBetween(string source, string destination, string intermediary, bool isAvoid, Dictionary<string, string[]> connections)
+            // if isAvoid, avoids intermediary, otherwise requires it
         {
-            Dictionary<string, string[]> connections = new Dictionary<string, string[]>(); // connections[source] == [destinations]
-            foreach (string line in data.Split(Environment.NewLine))
-            {
-                string source = line.Split(": ")[0];
-                string destinationBits = line.Split(": ")[1];
-                string[] destinations = destinationBits.Split(" ");
-                connections[source] = destinations;
-            }
-            List<List<string>> paths = new List<List<string>>() { new List<string>() { "you" } };
+            List<List<string>> paths = new List<List<string>>() { new List<string>() { source } };
             int finishedPaths = 0;
             while (paths.Count > 0)
             {
@@ -24,46 +15,21 @@ namespace AOC2025Day11
                     string currentPlace = path.Last();
                     foreach (string nextPlace in connections[currentPlace])
                     {
-                        if (nextPlace == "out")
+                        if (nextPlace == destination)
                         {
-                            finishedPaths++;
-                        }
-                        else
-                        {
-                            List<string> newPath = path.Append(nextPlace).ToList();
-                            newPaths.Add(newPath);
-                        }
-                    }
-                }
-                paths = new List<List<string>>(newPaths);
-            }
-            return Convert.ToString(finishedPaths);
-        }
-        public static string PartTwo(string data)
-        {
-            Dictionary<string, string[]> connections = new Dictionary<string, string[]>(); // connections[source] == [destinations]
-            foreach (string line in data.Split(Environment.NewLine))
-            {
-                string source = line.Split(": ")[0];
-                string destinationBits = line.Split(": ")[1];
-                string[] destinations = destinationBits.Split(" ");
-                connections[source] = destinations;
-            }
-            List<List<string>> paths = new List<List<string>>() { new List<string>() { "svr" } };
-            int finishedPaths = 0;
-            while (paths.Count > 0)
-            {
-                List<List<string>> newPaths = new List<List<string>>();
-                foreach (List<string> path in paths)
-                {
-                    string currentPlace = path.Last();
-                    foreach (string nextPlace in connections[currentPlace])
-                    {
-                        if (nextPlace == "out")
-                        {
-                            if (path.Contains("fft") && path.Contains("dac"))
+                            if (isAvoid)
                             {
-                                finishedPaths++;
+                                if (intermediary != destination & !path.Contains(intermediary)) // Destination is never added to path, so need to check
+                                {
+                                    finishedPaths++;
+                                }
+                            }
+                            else
+                            {
+                                if (intermediary == destination || path.Contains(intermediary)) // Destination is never added to path, so need to check
+                                {
+                                    finishedPaths++;
+                                }
                             }
                         }
                         else
@@ -75,10 +41,65 @@ namespace AOC2025Day11
                 }
                 paths = new List<List<string>>(newPaths);
             }
-            return Convert.ToString(finishedPaths);
+            return finishedPaths;
+        }
+        public static string PartOne(string data)
+        {
+            Dictionary<string, string[]> connections = new Dictionary<string, string[]>(); // connections[source] == [destinations]
+            foreach (string line in data.Split(Environment.NewLine))
+            {
+                string source = line.Split(": ")[0];
+                string destinationBits = line.Split(": ")[1];
+                string[] destinations = destinationBits.Split(" ");
+                connections[source] = destinations;
+            }
+            return Convert.ToString(NumberOfRoutesBetween("you", "out", "you", false, connections));
+        }
+        public static string PartTwo(string data)
+        {
+            Dictionary<string, string[]> connections = new Dictionary<string, string[]>(); // connections[source] == [destinations]
+            foreach (string line in data.Split(Environment.NewLine))
+            {
+                string source = line.Split(": ")[0];
+                string destinationBits = line.Split(": ")[1];
+                string[] destinations = destinationBits.Split(" ");
+                connections[source] = destinations;
+            }
+            //List<List<string>> paths = new List<List<string>>() { new List<string>() { "svr" } };
+            //int finishedPaths = 0;
+            //while (paths.Count > 0)
+            //{
+            //    List<List<string>> newPaths = new List<List<string>>();
+            //    foreach (List<string> path in paths)
+            //    {
+            //        string currentPlace = path.Last();
+            //        foreach (string nextPlace in connections[currentPlace])
+            //        {
+            //            if (nextPlace == "out")
+            //            {
+            //                if (path.Contains("fft") && path.Contains("dac"))
+            //                {
+            //                    finishedPaths++;
+            //                }
+            //            }
+            //            else
+            //            {
+            //                List<string> newPath = path.Append(nextPlace).ToList();
+            //                newPaths.Add(newPath); // Too memory intensive for laptop
+            //            }
+            //        }
+            //    }
+            //    paths = new List<List<string>>(newPaths);
+            //}
+            //return Convert.ToString(finishedPaths);
             // The number of paths from svr to out via fft and dac =
             //    (number from svr to dac via fft) * (number from dac to out)
-            //  + (number from svc to dac not via fft) * (number from dac to out via fft)
+            //  + (number from svr to dac not via fft) * (number from dac to out via fft)
+            int svr_to_dac_via_fft = NumberOfRoutesBetween("svr", "dac", "fft", false, connections);
+            int dac_to_out = NumberOfRoutesBetween("dac", "out", "dac", false, connections);
+            int svr_to_dac_not_via_fft = NumberOfRoutesBetween("svr", "dac", "fft", true, connections);
+            int dac_to_out_via_fft = NumberOfRoutesBetween("dac", "out", "fft", false, connections);
+            return Convert.ToString(svr_to_dac_via_fft * dac_to_out + svr_to_dac_not_via_fft * dac_to_out_via_fft);
         }
         static void Main()
         {
